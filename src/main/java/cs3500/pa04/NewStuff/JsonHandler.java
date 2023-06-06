4a40d2bff773ea648e701c9af752fe9b2077fb41
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2023-06-06 18:46:07 -0400

    Created the createCoordJson() method and the createVolleyJson method in order to create the commonly used Json objects as listed in the Notion documentation.

--- src/main/java/cs3500/pa04/NewStuff/JsonHandler.java
+++ src/main/java/cs3500/pa04/NewStuff/JsonHandler.java
@@ -2,9 +2,13 @@ package cs3500.pa04.NewStuff;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import cs3500.pa04.model.Coord;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class JsonHandler {
 
@@ -14,19 +18,58 @@ public class JsonHandler {
    * @return json object to be sent back to the server
    * @throws JsonProcessingException
    */
-  public JsonNode handleJoinRequest(String json) throws JsonProcessingException {
+  public ObjectNode handleJoinRequest(String json) throws JsonProcessingException {
     ObjectMapper mapper = new ObjectMapper();
-    ObjectNode joinRequest = mapper.createObjectNode();
+    ObjectNode joinRequest = mapper.createObjectNode(); // the main json node that will eventually be returned.
 
-    joinRequest.put("method-name","join");
-    ObjectNode arguments = mapper.createObjectNode();
-    arguments.put("name","shoumik123majumdar");
-    arguments.put("game-type","SINGLE");
+    joinRequest.put("method-name","join"); // adds the method-name field in the Json object
 
-    joinRequest.put("arguments",arguments);
+    ObjectNode arguments = mapper.createObjectNode(); // adds another nested layer to the Json object for the arguments
+    arguments.put("name","shoumik123majumdar"); // adds our github username as the name
+    arguments.put("game-type","SINGLE"); // initializes our gametype to be single
+
+    joinRequest.set("arguments",arguments);
 
     return joinRequest;
   }
+
+  /**
+   * Converts a coordinate into a Json Object
+   * @param coord coordinate to be converted
+   * @return coordinate as a Json Object
+   */
+  public ObjectNode createCoordJson(Coord coord){
+    ObjectMapper mapper = new ObjectMapper();
+    ObjectNode coordinate = mapper.createObjectNode();
+
+    String xCoordinate = String.valueOf(coord.getCoordinateX());
+    String yCoordinate = String.valueOf(coord.getCoordinateY());
+
+    coordinate.put("x",xCoordinate);
+    coordinate.put("y",yCoordinate);
+
+    return coordinate;
+  }
+
+  /**
+   * Creates an ArrayNode object to represent a volley of shots
+   * @param shotVolley the List of all of the volley's coordinates.
+   * @returns an ArrayNode object
+   */
+  public ArrayNode createVolleyJson(ArrayList<Coord> shotVolley){
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNodeFactory jsonNodeFactory = mapper.getNodeFactory();
+
+    ArrayNode volleyArrayNode = jsonNodeFactory.arrayNode();
+
+    for(Coord coord: shotVolley)
+    {
+      ObjectNode coordinate = createCoordJson(coord);
+      volleyArrayNode.add(coordinate);
+    }
+    return volleyArrayNode;
+  }
+
 
 
   public void handleJson(String json) {
